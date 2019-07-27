fd3d12996c83e6c15d129355dc4082e9c4f54eff
Doyeong Kim <jeekc0308@gmail.com>
2019-07-27 15:38:59 +0900

    fix

--- src/lib/errors/NotFoundError.js
+++ src/lib/errors/NotFoundError.js
@@ -1,6 +1,6 @@
 export default class NotFoundError extends Error {
   constructor() {
-    this.message = "페이지를 찾을 수 없습니다.";
+    super("페이지를 찾을 수 없습니다.");
     this.name = "NotFoundError";
     this.status = 404;
   }
--- src/lib/errors/ServerError.js
+++ src/lib/errors/ServerError.js
@@ -1,7 +1,7 @@
 export default class ServerError extends Error {
   constructor() {
+    super("서버에서 오류가 발생했습니다.");
     this.name = "ServerError";
-    this.message = "서버에서 오류가 발생했습니다.";
     this.statusCode = 500;
   }
 }
